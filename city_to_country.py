d6d6b49c1aaeb8bcbbdc12709472dada72f52194
sxvxmx <mr.evgexa.6000@gmail.com>
2026-03-20 22:24:08 +0300

    finished analysis

---
+++ city_to_country.py
@@ -0,0 +1,329 @@
+# City to Country mapping dictionary
+city_to_country = {
+    # Россия
+    'Moscow': 'Russia', 'Saint Petersburg': 'Russia', 'Sochi': 'Russia',
+    'Tula': 'Russia', 'Penza': 'Russia', 'Gelendzhik': 'Russia',
+    'Vilnius': 'Russia', 'Irkutsk': 'Russia', 'Vladivostok': 'Russia',
+    'Novorossiysk': 'Russia', 'Sergiev Posad': 'Russia', 'Ivanovo': 'Russia',
+    'Olkhon': 'Russia', 'Petrozavodsk': 'Russia', 'Adler': 'Russia',
+    'Kaliningrad': 'Russia', 'Ulyanovsk': 'Russia', 'Pyatigorsk': 'Russia',
+    'Nizhny Novgorod': 'Russia', 'Vyborg': 'Russia', 'Gaspra': 'Russia',
+    'Evpatoria': 'Russia', 'Novosibirsk': 'Russia', 'Kolomna': 'Russia',
+    'Abakan': 'Russia', 'Nalchik': 'Russia', 'Yoshkar-Ola': 'Russia',
+    'Tolyatti': 'Russia', 'Anapa': 'Russia', 'Vladimir': 'Russia',
+    'Sortavala': 'Russia', 'Kislovodsk': 'Russia', 'Vologda': 'Russia',
+    'Vladikavkaz': 'Russia', 'Kamchatka': 'Russia', 'Svetlogorsk': 'Russia',
+    'Baikal': 'Russia', 'Lazarevskoye': 'Russia', 'Arkhangelsk': 'Russia',
+    'Suzdal': 'Russia', 'Krasnodar': 'Russia', 'Sevastopol': 'Russia',
+    'Zelenogradsk': 'Russia', 'Makhachkala': 'Russia', 'Ekaterinburg': 'Russia',
+    'Taganrog': 'Russia', 'Perm': 'Russia', 'Partenit': 'Russia',
+    'Izberbash': 'Russia', 'Zheleznovodsk': 'Russia', 'Omsk': 'Russia',
+    'Yaroslavl': 'Russia', 'Tomsk': 'Russia', 'Smolensk': 'Russia',
+    'Torzhok': 'Russia', 'Yalta': 'Russia', 'Ufa': 'Russia',
+    'Yessentuki': 'Russia', 'Rostov-on-Don': 'Russia', 'Petropavlovsk-Kamchatsky': 'Russia',
+    'Gorno-Altaysk': 'Russia', 'Ryazan': 'Russia', 'Listvyanka': 'Russia',
+    'Saratov': 'Russia', 'Volgograd': 'Russia', 'Bryansk': 'Russia',
+    'Murmansk': 'Russia', 'Kazan': 'Russia', 'Feodosiya': 'Russia',
+    'Simeiz': 'Russia', 'Zvenigorod': 'Russia', 'Kostroma': 'Russia',
+    'Chelyabinsk': 'Russia', 'Bakhchisaray': 'Russia', 'Mineralnye Vody': 'Russia',
+    'Grozny': 'Russia', 'Foros': 'Russia', 'Alushta': 'Russia',
+    'Mezmay': 'Russia', 'Rybinsk': 'Russia', 'Miass': 'Russia',
+    'Pskov': 'Russia', 'Veliky Novgorod': 'Russia', 'Myshkin': 'Russia',
+    'Oryol': 'Russia', 'Tyumen': 'Russia', 'Ulan-Ude': 'Russia',
+    'Innopolis': 'Russia', 'Krasnoyarsk': 'Russia', 'Izhevsk': 'Russia',
+    'Kirov': 'Russia', 'Astrakhan': 'Russia', 'Kerch': 'Russia',
+    'Kaspiysk': 'Russia', 'Plyos': 'Russia', 'Staraya Russa': 'Russia',
+    'Kursk': 'Russia', 'Lipetsk': 'Russia', 'Simferopol': 'Russia',
+    'Derbent': 'Russia', 'Pereslavl-Zalessky': 'Russia', 'Belgorod': 'Russia',
+    'Kronstadt': 'Russia', 'Dombay': 'Russia', 'Pavlovsk': 'Russia',
+    'Voronezh': 'Russia', 'Rostov': 'Russia', 'Koktebel': 'Russia',
+    'New Athos': 'Russia', 'Ivangorod': 'Russia', 'Khanty-Mansiysk': 'Russia',
+    'Sudak': 'Russia', 'Tambov': 'Russia', 'Murom': 'Russia',
+    'zaraysk': 'Russia', 'Rzhev': 'Russia', 'Elista': 'Russia',
+    'bogolyubovo': 'Russia', 'Kabardinka': 'Russia', 'Sovetsk': 'Russia',
+    'chernyakhovsk': 'Russia', 'Yelets': 'Russia', 'Baikalsk': 'Russia',
+    'Gorodets': 'Russia', 'Chita': 'Russia', 'Dmitrov': 'Russia',
+    'Teriberka': 'Russia', 'Sarapul': 'Russia', 'Bagrationovsk': 'Russia',
+    'Orekhovo-Zuyevo': 'Russia', 'Zhukovsky': 'Russia', 'Tikhvin': 'Russia',
+    'Nikolayevka': 'Russia', 'Kungur': 'Russia', 'Izborsk': 'Russia',
+    'Klin': 'Russia', 'baltiysk': 'Russia', 'Serpukhov': 'Russia',
+    'Tuapse': 'Russia', 'Gurzuf': 'Russia', 'Maykop': 'Russia',
+    'Surgut': 'Russia', 'Kemerovo': 'Russia', 'Nevyansk': 'Russia',
+    'Kurgan': 'Russia', 'Magas': 'Russia', 'Aleksandrov': 'Russia',
+    'Arzamas': 'Russia', 'Saransk': 'Russia', 'Dzhubga': 'Russia',
+    'Stavropol': 'Russia', 'Khabarovsk': 'Russia', 'Saki': 'Russia',
+    'Abrau-Durso': 'Russia', 'Pitsunda': 'Russia', 'Polotsk': 'Russia',
+    'Chemal': 'Russia', 'Cheboksary': 'Russia', 'Orenburg': 'Russia',
+    'Yeysk': 'Russia', 'Temryuk': 'Russia', 'Arkhangelskoye': 'Russia',
+    'Kasimov': 'Russia', 'Alupka': 'Russia', 'Novokuznetsk': 'Russia',
+    'Nizhnevartovsk': 'Russia', 'Veliky Ustyug': 'Russia', 'Kalyazin': 'Russia',
+    'Sviyazhsk': 'Russia', 'Barnaul': 'Russia', 'Belokurikha': 'Russia',
+    'service-city': 'Russia', 'podolsk': 'Russia', 'Leningrad Oblast': 'Russia',
+    'Abkhazia': 'Georgia',
+    
+    # Грузия
+    'Tbilisi': 'Georgia', 'Batumi': 'Georgia', 'Gagra': 'Georgia',
+    'Kutaisi': 'Georgia', 'Mtskheta': 'Georgia', 'Kazbegi': 'Georgia',
+    'Kobuleti': 'Georgia', 'Sukhumi': 'Georgia',
+    
+    # Украина
+    'Kyiv': 'Ukraine', 'Lviv': 'Ukraine', 'Odessa': 'Ukraine',
+    'Kharkiv': 'Ukraine',
+    
+    # Беларусь
+    'Minsk': 'Belarus', 'Brest': 'Belarus', 'Gomel': 'Belarus',
+    'Grodno': 'Belarus',
+    
+    # Казахстан
+    'Almaty': 'Kazakhstan', 'Astana': 'Kazakhstan',
+    
+    # Узбекистан
+    'Tashkent': 'Uzbekistan', 'Samarkand': 'Uzbekistan', 'Bukhara': 'Uzbekistan',
+    
+    # Азербайджан
+    'Baku': 'Azerbaijan',
+    
+    # Армения
+    'Yerevan': 'Armenia',
+    
+    # Турция
+    'Istanbul': 'Turkey', 'Antalya': 'Turkey', 'Alanya': 'Turkey',
+    'Bodrum': 'Turkey', 'Marmaris': 'Turkey', 'Kusadasi': 'Turkey',
+    'Side': 'Turkey', 'Kemer': 'Turkey', 'Fethiye': 'Turkey', 'Belek': 'Turkey',
+    'Trabzon': 'Turkey', 'Dalaman': 'Turkey',
+    
+    # Египет
+    'Sharm-el-Sheikh': 'Egypt', 'Hurghada': 'Egypt', 'Cairo': 'Egypt',
+    
+    # ОАЭ
+    'Dubai': 'UAE', 'Abu Dhabi': 'UAE', 'Sharjah': 'UAE',
+    'Fujairah': 'UAE',
+    
+    # Катар
+    'Doha': 'Qatar',
+    
+    # Таиланд
+    'Phuket': 'Thailand', 'Pattaya': 'Thailand', 'Bangkok': 'Thailand',
+    'Samui': 'Thailand', 'Krabi': 'Thailand',
+    
+    # Вьетнам
+    'Nha Trang': 'Vietnam',
+    
+    # Индия
+    'Goa': 'India', 'Mumbai': 'India', 'Delhi': 'India',
+    
+    # Индонезия
+    'Bali': 'Indonesia', 'Jakarta': 'Indonesia',
+    
+    # Китай
+    'Beijing': 'China', 'Shanghai': 'China', 'Guangzhou': 'China',
+    'Lhasa': 'China',
+    
+    # Япония
+    'Tokyo': 'Japan', 'Yokohama': 'Japan', 'Kyoto': 'Japan',
+    
+    # Южная Корея
+    'Seoul': 'South Korea',
+    
+    # Сингапур
+    'Singapore': 'Singapore',
+    
+    # Малайзия
+    'Kuala Lumpur': 'Malaysia',
+    
+    # Кипр
+    'Ayia Napa': 'Cyprus', 'Protaras': 'Cyprus', 'Limassol': 'Cyprus',
+    'Paphos': 'Cyprus', 'Larnaca': 'Cyprus', 'Nicosia': 'Cyprus',
+    'Cyprus': 'Cyprus',
+    
+    # Греция
+    'Athens': 'Greece', 'Thessaloniki': 'Greece', 'Crete': 'Greece',
+    'Santorini': 'Greece', 'Corfu': 'Greece', 'Rodos': 'Greece',
+    'Rethymno': 'Greece', 'Halkidiki': 'Greece', 'Herceg Novi': 'Greece',
+    
+    # Испания
+    'Barcelona': 'Spain', 'Madrid': 'Spain', 'Seville': 'Spain',
+    'Valencia': 'Spain', 'Malaga': 'Spain', 'Bilbao': 'Spain',
+    'Granada': 'Spain', 'Tenerife': 'Spain', 'Ibiza': 'Spain',
+    'Majorca': 'Spain', 'Lloret de Mar': 'Spain', 'Cadiz': 'Spain',
+    'Tarragona': 'Spain', 'Alicante': 'Spain', 'Marbella': 'Spain',
+    'San Sebastian': 'Spain', 'Costa Brava': 'Spain', 'Costa Dorada': 'Spain',
+    'Las-Palmas-de-Gran-Canaria': 'Spain',
+    
+    # Португалия
+    'Lisbon': 'Portugal', 'Porto': 'Portugal', 'Madeira': 'Portugal',
+    'Funchal': 'Portugal', 'Portim': 'Portugal',
+    
+    # Италия
+    'Rome': 'Italy', 'Florence': 'Italy', 'Venice': 'Italy',
+    'Milan': 'Italy', 'Naples': 'Italy', 'Verona': 'Italy', 'Genoa': 'Italy',
+    'Bologna': 'Italy', 'Turin': 'Italy', 'Palermo': 'Italy', 'Catania': 'Italy',
+    'Syracuse': 'Italy', 'Bari': 'Italy', 'Pisa': 'Italy', 'Siena': 'Italy',
+    'Amalfi': 'Italy', 'Rimini': 'Italy', 'Bergamo': 'Italy', 'Parma': 'Italy',
+    'Bamberg': 'Italy', 'Spezia': 'Italy', 'Cagliari': 'Italy',
+    
+    # Франция
+    'Paris': 'France', 'Nice': 'France', 'Cannes': 'France',
+    'Lyon': 'France', 'Marseille': 'France', 'Versailles': 'France',
+    'Deauville': 'France', 'Chambery': 'France', 'Toulouse': 'France',
+    'Amboise': 'France',
+    
+    # Германия
+    'Berlin': 'Germany', 'Munich': 'Germany', 'Hamburg': 'Germany',
+    'Frankfurt': 'Germany', 'Cologne': 'Germany', 'Dresden': 'Germany',
+    'Leipzig': 'Germany', 'Hannover': 'Germany', 'Bonn': 'Germany',
+    'Dusseldorf': 'Germany', 'Baden-Baden': 'Germany', 'Aachen': 'Germany',
+    'Marburg': 'Germany', 'Bremen': 'Germany',
+    
+    # Австрия
+    'Innsbruck': 'Austria', 'Salzburg': 'Austria', 'Vienna': 'Austria',
+    'Tyrol': 'Austria',
+    
+    # Нидерланды
+    'Amsterdam': 'Netherlands', 'Rotterdam': 'Netherlands',
+    'The Hague': 'Netherlands', 'Volendam': 'Netherlands',
+    'Hertogenbosch': 'Netherlands',
+    
+    # Бельгия
+    'Brussels': 'Belgium', 'Bruges': 'Belgium', 'Antwerp': 'Belgium',
+    
+    # Швейцария
+    'Zurich': 'Switzerland', 'Geneva': 'Switzerland', 'Lucerne': 'Switzerland',
+    'Basel': 'Switzerland', 'Bern': 'Switzerland',
+    
+    # Великобритания
+    'London': 'UK', 'Edinburgh': 'UK', 'Manchester': 'UK',
+    'Liverpool': 'UK', 'Glasgow': 'UK', 'Scotland': 'UK',
+    
+    # Чехия
+    'Prague': 'Czech Republic', 'Karlovy Vary': 'Czech Republic',
+    'Hlubok': 'Czech Republic',
+    
+    # Польша
+    'Krakow': 'Poland', 'Warsaw': 'Poland', 'Gdansk': 'Poland',
+    
+    # Венгрия
+    'Budapest': 'Hungary', 'Hévíz': 'Hungary',
+    
+    # Хорватия
+    'Dubrovnik': 'Croatia', 'Split': 'Croatia', 'Zagreb': 'Croatia',
+    'Pula': 'Croatia', 'Rovinj': 'Croatia',
+    
+    # Словения
+    'Ljubljana': 'Slovenia',
+    
+    # Сербия
+    'Belgrade': 'Serbia',
+    
+    # Черногория
+    'Budva': 'Montenegro', 'Kotor': 'Montenegro', 'Zabljak': 'Montenegro',
+    
+    # Болгария
+    'Sofia': 'Bulgaria', 'Varna': 'Bulgaria', 'Bourgas': 'Bulgaria',
+    
+    # Румыния
+    'Bucharest': 'Romania', 'Brasov': 'Romania',
+    
+    # Литва
+    'Vilnius': 'Lithuania', 'Kaunas': 'Lithuania', 'Klaipeda': 'Lithuania',
+    
+    # Латвия
+    'Riga': 'Latvia', 'Jurmala': 'Latvia',
+    
+    # Эстония
+    'Tallinn': 'Estonia', 'Tartu': 'Estonia',
+    
+    # Финляндия
+    'Helsinki': 'Finland',
+    
+    # Швеция
+    'Stockholm': 'Sweden',
+    
+    # Норвегия
+    'Oslo': 'Norway', 'Geiranger': 'Norway',
+    
+    # Дания
+    'Copenhagen': 'Denmark',
+    
+    # Израиль
+    'Tel Aviv': 'Israel', 'Jerusalem': 'Israel', 'Haifa': 'Israel',
+    'Eilat': 'Israel', 'Netanya': 'Israel', 'Herzliya': 'Israel',
+    'Nazareth': 'Israel', 'Akko': 'Israel', 'Dead Sea': 'Israel',
+    
+    # Палестина
+    'Bethlehem': 'Palestine',
+    
+    # Тунис
+    'Tunis': 'Tunisia', 'Hammamet': 'Tunisia',
+    
+    # Марокко
+    'Marrakesh': 'Morocco', 'Fez': 'Morocco', 'Casablanca': 'Morocco',
+    
+    # ЮАР
+    'Cape Town': 'South Africa', 'Durban': 'South Africa',
+    
+    # Кения
+    'Nairobi': 'Kenya', 'Mombasa': 'Kenya', 'Diani': 'Kenya',
+    
+    # Уганда
+    'Uganda': 'Uganda', 'Kampala': 'Uganda',
+    
+    # Мексика
+    'Cancun': 'Mexico', 'Mexico': 'Mexico', 'Playa del Carmen': 'Mexico',
+    'Palenque': 'Mexico',
+    
+    # США
+    'New York': 'USA', 'Los Angeles': 'USA', 'Miami': 'USA',
+    'Las Vegas': 'USA', 'San Francisco': 'USA', 'Chicago': 'USA',
+    'Washington': 'USA', 'Boston': 'USA', 'Seattle': 'USA',
+    'Honolulu': 'USA',
+    
+    # Канада
+    'Montreal': 'Canada', 'Toronto': 'Canada', 'Vancouver': 'Canada',
+    
+    # Бразилия
+    'Rio de Janeiro': 'Brazil', 'Sao Paulo': 'Brazil',
+    
+    # Аргентина
+    'Buenos Aires': 'Argentina',
+    
+    # Колумбия
+    'Medellin': 'Colombia', 'Bogota': 'Colombia', 'Cartagena de Indias': 'Colombia',
+    
+    # Куба
+    'Havana': 'Cuba', 'Varadero': 'Cuba',
+    
+    # Доминикана
+    'Punta Cana': 'Dominican Republic', 'Boca Chica': 'Dominican Republic',
+    
+    # Австралия и Новая Зеландия
+    'Sydney': 'Australia', 'Melbourne': 'Australia',
+    'Auckland': 'New Zealand', 'Dunedin': 'New Zealand',
+    
+    # Камбоджа
+    'Siem Reap': 'Cambodia',
+    
+    # Шри-Ланка
+    'Sri Lanka': 'Sri Lanka',
+    
+    # Мальта
+    'Malta': 'Malta',
+    
+    # Монако
+    'Principality of Monaco': 'Monaco',
+    
+    # Молдова
+    'Chisinau': 'Moldova',
+    
+    # Уругвай
+    'Colonia del Sacramento': 'Uruguay',
+    
+    # Пуэрто-Мадрин
+    'Puerto Madryn': 'Argentina',
+    
+    # Гонконг
+    'Hong Kong': 'Hong Kong',
+    
+    # Тест
+    'test2': 'Unknown'
+}
